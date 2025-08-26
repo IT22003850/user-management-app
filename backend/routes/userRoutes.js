7ae794e42ecc4457c0b53637e76b1f7b3f8db363
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-26 12:25:25 +0530

    impliment routes for update and delete User

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,5 +6,7 @@ const authenticateToken = require('../middleware/authMiddleware');
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.get('/users', authenticateToken, userController.getUsers);
+router.put('/users/:id', authenticateToken, userController.updateUser)
+router.delete('/users/:id', authenticateToken, userController.deleteUser)
 
 module.exports = router;
